fd907e107d5b7b99f668bc0bd75edfc417b7e554
Beluga-Whale <savaddee159@gmail.com>
2025-02-26 22:05:15 +0700

    feat: add input for search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import SearchForm from "../components/SearchForm";
+
+export default function Home({}) {
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get("query");
+
   return (
     <>
-      <h1>HOME</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas. and Get Noticed in Virtual Cometitions
+        </p>
+        <SearchForm query={query ?? ""} />
+      </section>
     </>
   );
 }
+// NOTE 1:50:59
